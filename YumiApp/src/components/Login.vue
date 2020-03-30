ef6603c5cb5ba17cc4eefcd1d1cf7c256d809d59
mictho18 <42611107+mictho18@users.noreply.github.com>
2020-03-30 15:48:48 +0200

    Update of component Login and Register

--- YumiApp/src/components/Login.vue
+++ YumiApp/src/components/Login.vue
@@ -1,9 +1,51 @@
 <template>
-    <div></div> 
+    <div class="text-md-center">
+        <h2>Login</h2>
+        <v-layout>
+            <v-flex xs6 offset-xs3>
+                <v-card>
+                    <v-card-text>
+                        <v-text-field
+                        v-model="login.email"
+                        label="Epost"
+                        required
+                        >
+                        </v-text-field>
+
+                        <v-text-field
+                        v-model="login.password"
+                        label="Passord"
+                        type="password"
+                        required
+                        >
+
+                        </v-text-field>
+                    </v-card-text>
+                    <v-btn rounded color="amber accent-3 black--text" class="ml-4" dark min-width="150px" depressed>Log in</v-btn>
+                    <v-btn rounded color="amber accent-3 black--text" class="ml-4" dark min-width="150px" depressed>Registrer deg</v-btn>
+                </v-card>
+            </v-flex>
+
+
+
+
+        </v-layout>
+        
+    
+    </div> 
 </template>
+
 <script>
 export default {
-    name: 'Login'
+    name: 'Login',
+    data(){
+        return{
+            login:{
+                email:"",
+                password:""
+            }
+        }
+    }
     
 }
 </script>
--- YumiApp/src/components/Register.vue
+++ YumiApp/src/components/Register.vue
@@ -1,9 +1,80 @@
 <template>
-    <div></div> 
+    <div>
+        <h2>Register</h2>
+        <v-layout>
+            <v-flex xs6 offset-xs3>
+                <v-card>
+                    <v-card-text>
+                        <v-text-field
+                        v-model="register.FirstName"
+                        label="Fornavn"
+                        required
+                        >
+                        
+                        </v-text-field>
+                        <v-text-field
+                        v-model="register.LastName"
+                        label="Etternavn"
+                        required
+                        >
+                        
+                        </v-text-field>
+                        <v-text-field
+                        v-model="register.adress"
+                        label="Adresse"
+                        required
+                        >
+                        
+                        </v-text-field>
+                        <v-text-field
+                        v-model="register.Phonenumber"
+                        label="Telefonnummer"
+                        required
+                        >
+                        
+                        </v-text-field>
+                        <v-text-field
+                        v-model="login.email"
+                        label="Epost"
+                        required
+                        >
+                        </v-text-field>
+
+                        <v-text-field
+                        v-model="login.password"
+                        label="Passord"
+                        type="password"
+                        required
+                        >
+
+                        </v-text-field>
+                    </v-card-text>
+                    <v-btn rounded color="amber accent-3 black--text" class="ml-4" dark min-width="150px" depressed>Registrer deg</v-btn>
+                </v-card>
+            </v-flex>
+
+
+
+
+        </v-layout>    
+        
+    </div> 
 </template>
 <script>
 export default {
-    name: 'Register'
+    name: 'Register',
+    data(){
+        return{
+            register:{
+                FirstName:"",
+                LastName:"",
+                Adress:"",
+                PhoneNumber:"",
+                Email:"",
+                Password:""
+            }
+        }
+    }
     
 }
 </script>
