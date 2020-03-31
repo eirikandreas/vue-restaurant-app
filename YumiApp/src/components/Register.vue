1afb0617834d975429d57c565805b5507b67b12e
mictho18 <42611107+mictho18@users.noreply.github.com>
2020-03-31 17:10:47 +0200

    Login Register Form
    Updatet to use Vuetify Form

--- YumiApp/src/components/Register.vue
+++ YumiApp/src/components/Register.vue
@@ -1,80 +1,100 @@
 <template>
     <div>
-        <h2>Register</h2>
-        <v-layout>
-            <v-flex xs6 offset-xs3>
-                <v-card>
-                    <v-card-text>
-                        <v-text-field
-                        v-model="register.FirstName"
-                        label="Fornavn"
-                        required
-                        >
-                        
-                        </v-text-field>
-                        <v-text-field
-                        v-model="register.LastName"
-                        label="Etternavn"
-                        required
-                        >
-                        
-                        </v-text-field>
-                        <v-text-field
-                        v-model="register.adress"
-                        label="Adresse"
-                        required
-                        >
-                        
-                        </v-text-field>
-                        <v-text-field
-                        v-model="register.Phonenumber"
-                        label="Telefonnummer"
-                        required
-                        >
-                        
-                        </v-text-field>
-                        <v-text-field
-                        v-model="login.email"
-                        label="Epost"
-                        required
-                        >
-                        </v-text-field>
+        <template>
+  <v-app id="Register">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="Black"
+                dark
+                flat
+              >
+                <v-toolbar-title>Ny bruker</v-toolbar-title>
+                <v-spacer />
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      :href="source"
+                      icon
+                      large
+                      target="_blank"
+                      v-on="on"
+                    >
+                    </v-btn>
+                  </template>
+               
+                
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Fornavn"
+                    name="FirstName"
+                    type="text"
+                  />
+                  <v-text-field
+                    label="Etternavn"
+                    name="LastName"
+                    type="text"
+                  />
 
-                        <v-text-field
-                        v-model="login.password"
-                        label="Passord"
-                        type="password"
-                        required
-                        >
+                  <v-text-field
+                    label="Adresse"
+                    name="Adress"
+                    type="text"
+                  />
+                  <v-text-field
+                    label="Telefonnummer"
+                    name="phonenumber"
+                    type="text"
+                  />
+                  <v-text-field
+                    label="Epost"
+                    name="email"
+                    type="text"
+                  />
+                  <v-text-field
+                    id="password"
+                    label="Passord"
+                    name="password"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="Yellow">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
 
-                        </v-text-field>
-                    </v-card-text>
-                    <v-btn rounded color="amber accent-3 black--text" class="ml-4" dark min-width="150px" depressed>Registrer deg</v-btn>
-                </v-card>
-            </v-flex>
-
-
-
-
-        </v-layout>    
+  
         
     </div> 
 </template>
+
 <script>
 export default {
     name: 'Register',
-    data(){
-        return{
-            register:{
-                FirstName:"",
-                LastName:"",
-                Adress:"",
-                PhoneNumber:"",
-                Email:"",
-                Password:""
-            }
-        }
-    }
     
 }
 </script>
