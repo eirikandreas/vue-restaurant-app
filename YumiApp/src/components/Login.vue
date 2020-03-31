632e72f84419ed0699109a0b2661b55d9d56b9e5
Eirik Andreas <eirikab@gmail.com>
2020-03-31 17:29:17 +0200

    Revert "Login Register Form"
    This reverts commit 1afb0617834d975429d57c565805b5507b67b12e.

--- YumiApp/src/components/Login.vue
+++ YumiApp/src/components/Login.vue
@@ -1,78 +1,51 @@
 <template>
-    <div>
-        <template>
-  <v-app id="Register">
-    <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-          >
-            <v-card class="elevation-12">
-              <v-toolbar
-                color="Black"
-                dark
-                flat
-              >
-                <v-toolbar-title>Login</v-toolbar-title>
-                <v-spacer />
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      :href="source"
-                      icon
-                      large
-                      target="_blank"
-                      v-on="on"
-                    >
-                    </v-btn>
-                  </template>
-               
-                
-                </v-tooltip>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="Epost"
-                    name="email"
-                    type="text"
-                  />
-                  <v-text-field
-                    id="password"
-                    label="Passord"
-                    name="password"
-                    type="password"
-                  />
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer />
-                <v-btn color="dark">Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-  </v-app>
-</template>
+    <div class="text-md-center">
+        <h2>Login</h2>
+        <v-layout>
+            <v-flex xs6 offset-xs3>
+                <v-card>
+                    <v-card-text>
+                        <v-text-field
+                        v-model="login.email"
+                        label="Epost"
+                        required
+                        >
+                        </v-text-field>
 
-  
+                        <v-text-field
+                        v-model="login.password"
+                        label="Passord"
+                        type="password"
+                        required
+                        >
+
+                        </v-text-field>
+                    </v-card-text>
+                    <v-btn rounded color="amber accent-3 black--text" class="ml-4" dark min-width="150px" depressed>Log in</v-btn>
+                    <v-btn rounded color="amber accent-3 black--text" class="ml-4" dark min-width="150px" depressed>Registrer deg</v-btn>
+                </v-card>
+            </v-flex>
+
+
+
+
+        </v-layout>
         
+    
     </div> 
 </template>
+
 <script>
 export default {
     name: 'Login',
+    data(){
+        return{
+            login:{
+                email:"",
+                password:""
+            }
+        }
+    }
     
 }
 </script>
