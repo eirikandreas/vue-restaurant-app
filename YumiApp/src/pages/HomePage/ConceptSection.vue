f18e774091a9cd747eeb141236a64e302ff29701
Eirik Andreas <eirikab@gmail.com>
2020-04-01 02:33:04 +0200

    Fix responsive Concept component

--- YumiApp/src/pages/HomePage/ConceptSection.vue
+++ YumiApp/src/pages/HomePage/ConceptSection.vue
@@ -2,8 +2,12 @@
 <section id="concept-section">
 <v-container>
   <v-row>
-    <v-col cols="12" sm="12" md="6">
-  <v-img contain :src="require(`@/assets/images/${sectionConceptImg}`)" height="600px"></v-img>
+    <v-col cols="12" sm="12" md="6" class="justify-center pa-0">
+  <v-img contain :src="require(`@/assets/images/${sectionConceptImg}`)" min-width="400px">
+  
+
+  
+  </v-img>
     </v-col>
 
     <v-col cols="12" sm="12" md="6" class="d-flex align-center">
@@ -12,7 +16,7 @@
       <v-card flat :class="{'text-center mb-6': $vuetify.breakpoint.smAndDown}" >
 
 
-        <v-card-title :class="[{'justify-center': $vuetify.breakpoint.smAndDown}, 'display-3', 'font-weight-bold']">Our concept</v-card-title>
+        <v-card-title :class="[{'justify-center display-2': $vuetify.breakpoint.smAndDown}, 'display-3', 'font-weight-bold']">Our concept</v-card-title>
         <v-card-text>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat cupiditate fugiat laboriosam porro officiis asperiores temporibus illo nulla aliquam culpa vitae assumenda facere harum a, ipsum sit iste velit possimus!
 
@@ -38,7 +42,7 @@ export default {
     name: 'ConceptSection',
      data () {
       return {
-        sectionConceptImg: 'concept-image-2.png',
+        sectionConceptImg: 'concept-image-1-crop.png',
         colors: [
           'indigo',
           'warning',
