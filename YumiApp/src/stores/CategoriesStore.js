2b930b823751d11e564961f947d155f642db8050
Eirik Andreas <eirikab@gmail.com>
2020-04-29 22:42:42 +0200

    Fix assets

--- YumiApp/src/stores/CategoriesStore.js
+++ YumiApp/src/stores/CategoriesStore.js
@@ -1,12 +1,12 @@
 const CategoriesStore = {
     state: {
         categories: [
-            { id: 1, name: "All", imgSrc: "1.jpg", blackIcon: "menu.png", whiteIcon: "menu.png" },
-            { id: 1, name: "Appetizers", imgSrc: "1.jpg", blackIcon: "appetizer.png", whiteIcon: "appetizer-w.png" },
-            { id: 3, name: "Maki", imgSrc: "2.jpg", blackIcon: "maki.png", whiteIcon: "maki-w.png" },
-            { id: 4, name: "Nigiri", imgSrc: "3.jpg", blackIcon: "nigiri.png", whiteIcon: "nigiri-w.png" },
-            { id: 5, name: "Sashimi", imgSrc: "4.jpg", blackIcon: "sashimi.png", whiteIcon: "sashimi-w.png" },
-            { id: 7, name: "Sushi", imgSrc: "1.jpg", blackIcon: "food.png", whiteIcon: "food.png" },
+            { id: 1, name: "All", imgSrc: "categories/1.jpg", blackIcon: "menu.png", whiteIcon: "menu.png" },
+            { id: 1, name: "Appetizers", imgSrc: "categories/1.jpg", blackIcon: "appetizer.png", whiteIcon: "appetizer-w.png" },
+            { id: 3, name: "Maki", imgSrc: "categories/2.jpg", blackIcon: "maki.png", whiteIcon: "maki-w.png" },
+            { id: 4, name: "Nigiri", imgSrc: "categories/3.jpg", blackIcon: "nigiri.png", whiteIcon: "nigiri-w.png" },
+            { id: 5, name: "Sashimi", imgSrc: "categories/4.jpg", blackIcon: "sashimi.png", whiteIcon: "sashimi-w.png" },
+            { id: 7, name: "Sushi", imgSrc: "categories1.jpg", blackIcon: "food.png", whiteIcon: "food.png" },
         ]
     },
     // Gir tilgang til å hente ut all data
